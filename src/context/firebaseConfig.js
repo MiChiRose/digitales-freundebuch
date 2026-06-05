ced92945dfbd9b7e9235116ae055a9771d37b2a4
Yury Menshchykau <yura.menschikov@icloud.com>
2026-06-05 13:17:03 +0300

    update firebase config

--- src/context/firebaseConfig.js
+++ src/context/firebaseConfig.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyA701EHLE4urYA0Qmr-C6mcAHDXXzxb6rI',
-  authDomain: 'digitales-freundebuch.firebaseapp.com',
-  projectId: 'digitales-freundebuch',
-  storageBucket: 'digitales-freundebuch.firebasestorage.app',
-  messagingSenderId: '891826792087',
-  appId: '1:891826792087:web:42def0c9625e3610d82bd2'
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID
 };
 
 let app;
@@ -34,4 +34,3 @@ try {
 // Export db and auth with safety checks
 export const db = isInitialized ? getFirestore(app) : null;
 export const auth = isInitialized ? getAuth(app) : null;
-
